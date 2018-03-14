41a54e779f3e940e673e65d814844993f4775db1
Nikita Provotorov <nikita.provotorov.work@gmail.com>
2018-03-15 05:56:02 +0700

    Refactor existing tests

--- tests/list.cpp
+++ tests/list.cpp
@@ -1,6 +1,7 @@
 #include <xor_list.h>
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
+#include <list>
 
 
 template<typename T>
@@ -52,7 +53,8 @@ TEST(LIST, CONSTRUCTOR_DEFAULT)
 {
     LinkedList<NonMovableValue<int>> list;
 
-    EXPECT_TRUE(list.empty());
+    ASSERT_TRUE(list.empty());
+    ASSERT_THAT(list, ::testing::ElementsAre());
 }
 
 
@@ -60,100 +62,59 @@ TEST(LIST, CONSTRUCTOR_ALLOCATOR)
 {
     LinkedList<NonMovableValue<int>> list(std::allocator<NonMovableValue<int>>{});
 
-    EXPECT_TRUE(list.empty());
-
-    list.emplace_back(10);
-    list.emplace_back(20);
-
-    EXPECT_EQ(list.size(), 2U);
-    EXPECT_EQ(list.front(), 10);
-    EXPECT_EQ(list.back(), 20);
+    ASSERT_TRUE(list.empty());
+    ASSERT_THAT(list, ::testing::ElementsAre());
 }
 
 
 TEST(LIST, CONSTRUCTOR_INITIALIZER_LIST)
 {
-    LinkedList<Value<int>> l1{-1, 0, 1, 2, 3, 4};
+    LinkedList<Value<int>> list{-1, 0, 1, 2, 3, 4};
 
-    EXPECT_EQ(l1.size(), 6U);
-
-    int i = -1;
-    for (int e : l1)
-    {
-        EXPECT_EQ(e, i++);
-    }
-    EXPECT_EQ(i, 5);
+    ASSERT_EQ(list.size(), 6U);
+    ASSERT_THAT(list, ::testing::ElementsAre(-1, 0, 1, 2, 3, 4));
 }
 
 TEST(LIST, CONSTRUCTOR_EMPTY_INITIALIZER_LIST)
 {
-    LinkedList<Value<int>> l1({}, std::allocator<Value<int>>{});
+    LinkedList<Value<int>> list({}, std::allocator<Value<int>>{});
 
-    EXPECT_TRUE(l1.empty());
-
-    for (int e : l1)
-    {
-        e = 0;
-        FAIL();
-    }
+    ASSERT_TRUE(list.empty());
+    ASSERT_THAT(list, ::testing::ElementsAre());
 }
 
 
 TEST(LIST, CONSTRUCTOR_DEFAULT_FILL)
 {
-    LinkedList<Value<int>> l1(10U);
+    LinkedList<Value<int>> list(10U);
 
-    EXPECT_EQ(l1.size(), 10U);
-
-    int i = 0;
-    for (int e : l1)
-    {
-        EXPECT_EQ(e, int{});
-        ++i;
-    }
-    EXPECT_EQ(i, 10);
+    ASSERT_EQ(list.size(), 10U);
+    ASSERT_THAT(list, ::testing::ElementsAre(int{}, int{}, int{}, int{}, int{}, int{}, int{}, int{}, int{}, int{}));
 }
 
 TEST(LIST, CONSTRUCTOR_EMPTY_DEFAULT_FILL)
 {
-    LinkedList<Value<int>> l1(0);
+    LinkedList<Value<int>> list(0);
 
-    EXPECT_TRUE(l1.empty());
-
-    for (int e : l1)
-    {
-        e = 0;
-        FAIL();
-    }
+    ASSERT_TRUE(list.empty());
+    ASSERT_THAT(list, ::testing::ElementsAre());
 }
 
 
 TEST(LIST, CONSTRUCTOR_VALUE_FILL)
 {
-    LinkedList<Value<int>> l1(10U, -1590);
+    LinkedList<Value<int>> list(10U, -1590);
 
-    EXPECT_EQ(l1.size(), 10U);
-
-    int i = 0;
-    for (int e : l1)
-    {
-        EXPECT_EQ(e, -1590);
-        ++i;
-    }
-    EXPECT_EQ(i, 10);
+    ASSERT_EQ(list.size(), 10U);
+    ASSERT_THAT(list, ::testing::ElementsAre(-1590, -1590, -1590, -1590, -1590, -1590, -1590, -1590, -1590, -1590));
 }
 
 TEST(LIST, CONSTRUCTOR_EMPTY_VALUE_FILL)
 {
-    LinkedList<Value<int>> l1(0U, -1590);
+    LinkedList<Value<int>> list(0U, -1590);
 
-    EXPECT_TRUE(l1.empty());
-
-    for (int e : l1)
-    {
-        e = 0;
-        FAIL();
-    }
+    ASSERT_TRUE(list.empty());
+    ASSERT_THAT(list, ::testing::ElementsAre());
 }
 
 
@@ -161,91 +122,58 @@ TEST(LIST, COPY_CONSTRUCTOR_EMPTY)
 {
     LinkedList<Value<int>> l1, l2(l1);
 
-    EXPECT_TRUE(l1.empty());
-    EXPECT_TRUE(l2.empty());
+    ASSERT_TRUE(l1.empty());
+    ASSERT_THAT(l1, ::testing::ElementsAre());
 
-    for (int e : l1)
-    {
-        FAIL();
-    }
-
-    for (int e : l2)
-    {
-        FAIL();
-    }
+    ASSERT_TRUE(l2.empty());
+    ASSERT_THAT(l2, ::testing::ElementsAre());
 }
 
 TEST(LIST, COPY_CONSTRUCTOR_NON_EMPTY)
 {
     LinkedList<Value<int>> l1{1, 2, 3, 4, 5, 6, 7, 8, 9}, l2(l1);
 
-    EXPECT_EQ(l1.size(), 9U);
-    EXPECT_EQ(l2.size(), l1.size());
+    ASSERT_EQ(l2.size(), 9U);
+    ASSERT_THAT(l2, ::testing::ElementsAre(1, 2, 3, 4, 5, 6, 7, 8, 9));
 
-    int i = 1;
-    auto it = l1.cbegin();
-    for (int e : l2)
-    {
-        EXPECT_EQ(e, *it++);
-        ++i;
-    }
+    l1.~LinkedList();
 
-    EXPECT_EQ(i, 10);
+    ASSERT_EQ(l2.size(), 9U);
+    ASSERT_THAT(l2, ::testing::ElementsAre(1, 2, 3, 4, 5, 6, 7, 8, 9));
 }
 
 TEST(LIST, MOVE_CONSTRUCTOR_EMPTY)
 {
     LinkedList<Value<int>> l1, l2(std::move(l1));
 
-    EXPECT_TRUE(l1.empty());
-    EXPECT_TRUE(l2.empty());
+    ASSERT_TRUE(l1.empty());
+    ASSERT_THAT(l1, ::testing::ElementsAre());
 
-    for (int e : l1)
-    {
-        FAIL();
-    }
-
-    for (int e : l2)
-    {
-        FAIL();
-    }
+    ASSERT_TRUE(l2.empty());
+    ASSERT_THAT(l2, ::testing::ElementsAre());
 }
 
 TEST(LIST, MOVE_CONSTRUCTOR_NON_EMPTY)
 {
-    LinkedList<Value<int>> l1{1, 2, 3, 4, 5, 6, 7, 8, 9}, l2(std::move(l1));
+    LinkedList<Value<int>> l1{1, 2, 3, 4, 5, 6, 7, 8, 9};
+    LinkedList<Value<int>> l2(std::move(l1));
 
-    EXPECT_TRUE(l1.empty());
-    for (int e : l1)
-    {
-        FAIL();
-    }
+    ASSERT_TRUE(l1.empty());
+    ASSERT_THAT(l1, ::testing::ElementsAre());
 
-    EXPECT_EQ(l2.size(), 9U);
-
-    int i = 1;
-    for (int e : l2)
-    {
-        EXPECT_EQ(e, i++);
-    }
-    EXPECT_EQ(i, 10);
+    ASSERT_EQ(l2.size(), 9U);
+    ASSERT_THAT(l2, ::testing::ElementsAre(1, 2, 3, 4, 5, 6, 7, 8, 9));
 }
 
 
 TEST(LIST, COPY_ASSIGNMENT_SELF)
 {
-    LinkedList<Value<int>> l1{1, 2, 3, 4, 5, 6, 7, 8, 9};
+    LinkedList<Value<int>> list{1, 2, 3, 4, 5, 6, 7, 8, 9};
 
-    l1 = l1;
+    list = list;
 
-    EXPECT_EQ(l1.size(), 9U);
-
-    int i = 1;
-    for (int e : l1)
-    {
-        EXPECT_EQ(e, i++);
-    }
-    EXPECT_EQ(i, 10);
+    ASSERT_EQ(list.size(), 9U);
+    ASSERT_THAT(list, ::testing::ElementsAre(1, 2, 3, 4, 5, 6, 7, 8, 9));
 }
 
 TEST(LIST, COPY_ASSIGNMENT_EMPTY)
@@ -254,11 +182,8 @@ TEST(LIST, COPY_ASSIGNMENT_EMPTY)
 
     l1 = l2;
 
-    EXPECT_TRUE(l1.empty());
-    for (int e : l1)
-    {
-        FAIL();
-    }
+    ASSERT_TRUE(l1.empty());
+    ASSERT_THAT(l1, ::testing::ElementsAre());
 }
 
 TEST(LIST, COPY_ASSIGNMENT_NON_EMPTY_LESS)
@@ -268,14 +193,8 @@ TEST(LIST, COPY_ASSIGNMENT_NON_EMPTY_LESS)
     l1 = l2;
     l2.clear();
 
-    EXPECT_EQ(l1.size(), 6U);
-
-    int i = 0;
-    for (int e : l1)
-    {
-        EXPECT_EQ(e, i += 10);
-    }
-    EXPECT_EQ(i, 60);
+    ASSERT_EQ(l1.size(), 6U);
+    ASSERT_THAT(l1, ::testing::ElementsAre(10, 20, 30, 40, 50, 60));
 }
 
 TEST(LIST, COPY_ASSIGNMENT_NON_EMPTY_GREATER)
@@ -285,31 +204,19 @@ TEST(LIST, COPY_ASSIGNMENT_NON_EMPTY_GREATER)
     l1 = l2;
     l2.clear();
 
-    EXPECT_EQ(l1.size(), 9U);
-
-    int i = 1;
-    for (int e : l1)
-    {
-        EXPECT_EQ(e, i++);
-    }
-    EXPECT_EQ(i, 10);
+    ASSERT_EQ(l1.size(), 9U);
+    ASSERT_THAT(l1, ::testing::ElementsAre(1, 2, 3, 4, 5, 6, 7, 8, 9));
 }
 
 
 TEST(LIST, MOVE_ASSIGNMENT_SELF)
 {
-    LinkedList<Value<int>> l1{1, 2, 3, 4, 5, 6, 7, 8, 9};
+    LinkedList<Value<int>> list{1, 2, 3, 4, 5, 6, 7, 8, 9};
 
-    l1 = std::move(l1);
+    list = std::move(list);
 
-    EXPECT_EQ(l1.size(), 9U);
-
-    int i = 1;
-    for (int e : l1)
-    {
-        EXPECT_EQ(e, i++);
-    }
-    EXPECT_EQ(i, 10);
+    ASSERT_EQ(list.size(), 9U);
+    ASSERT_THAT(list, ::testing::ElementsAre(1, 2, 3, 4, 5, 6, 7, 8, 9));
 }
 
 TEST(LIST, MOVE_ASSIGNMENT_EMPTY)
@@ -318,17 +225,11 @@ TEST(LIST, MOVE_ASSIGNMENT_EMPTY)
 
     l1 = std::move(l2);
 
-    EXPECT_TRUE(l2.empty());
-    for (int e : l2)
-    {
-        FAIL();
-    }
+    ASSERT_TRUE(l2.empty());
+    ASSERT_THAT(l2, ::testing::ElementsAre());
 
-    EXPECT_TRUE(l1.empty());
-    for (int e : l1)
-    {
-        FAIL();
-    }
+    ASSERT_TRUE(l1.empty());
+    ASSERT_THAT(l1, ::testing::ElementsAre());
 }
 
 TEST(LIST, MOVE_ASSIGNMENT_NON_EMPTY_LESS)
@@ -337,20 +238,11 @@ TEST(LIST, MOVE_ASSIGNMENT_NON_EMPTY_LESS)
 
     l1 = std::move(l2);
 
-    EXPECT_TRUE(l2.empty());
-    for (int e : l2)
-    {
-        FAIL();
-    }
+    ASSERT_TRUE(l2.empty());
+    ASSERT_THAT(l2, ::testing::ElementsAre());
 
-    EXPECT_EQ(l1.size(), 6U);
-
-    int i = 0;
-    for (int e : l1)
-    {
-        EXPECT_EQ(e, i += 10);
-    }
-    EXPECT_EQ(i, 60);
+    ASSERT_EQ(l1.size(), 6U);
+    ASSERT_THAT(l1, ::testing::ElementsAre(10, 20, 30, 40, 50, 60));
 }
 
 TEST(LIST, MOVE_ASSIGNMENT_NON_EMPTY_GREATER)
@@ -359,20 +251,11 @@ TEST(LIST, MOVE_ASSIGNMENT_NON_EMPTY_GREATER)
 
     l1 = std::move(l2);
 
-    EXPECT_TRUE(l2.empty());
-    for (int e : l2)
-    {
-        FAIL();
-    }
+    ASSERT_TRUE(l2.empty());
+    ASSERT_THAT(l2, ::testing::ElementsAre());
 
-    EXPECT_EQ(l1.size(), 9U);
-
-    int i = 1;
-    for (int e : l1)
-    {
-        EXPECT_EQ(e, i++);
-    }
-    EXPECT_EQ(i, 10);
+    ASSERT_EQ(l1.size(), 9U);
+    ASSERT_THAT(l1, ::testing::ElementsAre(1, 2, 3, 4, 5, 6, 7, 8, 9));
 }
 
 
@@ -382,8 +265,11 @@ TEST(LIST, MERGE_BOTH_EMPTY)
 
     l2.merge(l1);
 
-    EXPECT_TRUE(l1.empty());
-    EXPECT_TRUE(l2.empty());
+    ASSERT_TRUE(l1.empty());
+    ASSERT_THAT(l1, ::testing::ElementsAre());
+
+    ASSERT_TRUE(l2.empty());
+    ASSERT_THAT(l2, ::testing::ElementsAre());
 }
 
 TEST(LIST, MERGE_SINGLE_TO_EMPTY)
@@ -392,9 +278,11 @@ TEST(LIST, MERGE_SINGLE_TO_EMPTY)
 
     l2.merge(l1);
 
-    EXPECT_TRUE(l1.empty());
-    EXPECT_EQ(l2.size(), 1U);
-    EXPECT_EQ(l2.front(), -10);
+    ASSERT_TRUE(l1.empty());
+    ASSERT_THAT(l1, ::testing::ElementsAre());
+
+    ASSERT_EQ(l2.size(), 1U);
+    ASSERT_THAT(l2, ::testing::ElementsAre(-10));
 }
 
 TEST(LIST, MERGE_EMPTY_TO_SINGLE)
@@ -403,9 +291,11 @@ TEST(LIST, MERGE_EMPTY_TO_SINGLE)
 
     l2.merge(l1);
 
-    EXPECT_TRUE(l1.empty());
-    EXPECT_EQ(l2.size(), 1U);
-    EXPECT_EQ(l2.front(), -10);
+    ASSERT_TRUE(l1.empty());
+    ASSERT_THAT(l1, ::testing::ElementsAre());
+
+    ASSERT_EQ(l2.size(), 1U);
+    ASSERT_THAT(l2, ::testing::ElementsAre(-10));
 }
 
 TEST(LIST, MERGE_SINGLES_WITHOUT_SWAP)
@@ -414,10 +304,11 @@ TEST(LIST, MERGE_SINGLES_WITHOUT_SWAP)
 
     l2.merge(l1);
 
-    EXPECT_TRUE(l1.empty());
-    EXPECT_EQ(l2.size(), 2U);
-    EXPECT_EQ(l2.front(), -10);
-    EXPECT_EQ(l2.back(), 10);
+    ASSERT_TRUE(l1.empty());
+    ASSERT_THAT(l1, ::testing::ElementsAre());
+
+    ASSERT_EQ(l2.size(), 2U);
+    ASSERT_THAT(l2, ::testing::ElementsAre(-10, 10));
 }
 
 TEST(LIST, MERGE_SINGLES_WITH_SWAP)
@@ -426,10 +317,11 @@ TEST(LIST, MERGE_SINGLES_WITH_SWAP)
 
     l2.merge(l1);
 
-    EXPECT_TRUE(l1.empty());
-    EXPECT_EQ(l2.size(), 2U);
-    EXPECT_EQ(l2.front(), -10);
-    EXPECT_EQ(l2.back(), 10);
+    ASSERT_TRUE(l1.empty());
+    ASSERT_THAT(l1, ::testing::ElementsAre());
+
+    ASSERT_EQ(l2.size(), 2U);
+    ASSERT_THAT(l2, ::testing::ElementsAre(-10, 10));
 }
 
 TEST(LIST, MERGE_SAME_SINGLES)
@@ -438,10 +330,11 @@ TEST(LIST, MERGE_SAME_SINGLES)
 
     l2.merge(l1);
 
-    EXPECT_TRUE(l1.empty());
-    EXPECT_EQ(l2.size(), 2U);
-    EXPECT_EQ(l2.front(), 10);
-    EXPECT_EQ(l2.back(), 10);
+    ASSERT_TRUE(l1.empty());
+    ASSERT_THAT(l1, ::testing::ElementsAre());
+
+    ASSERT_EQ(l2.size(), 2U);
+    ASSERT_THAT(l2, ::testing::ElementsAre(10, 10));
 }
 
 TEST(LIST, MERGE_WITHOUT_SWAP)
@@ -450,13 +343,11 @@ TEST(LIST, MERGE_WITHOUT_SWAP)
 
     l2.merge(l1);
 
-    EXPECT_TRUE(l1.empty());
-    EXPECT_EQ(l2.size(), 8U);
+    ASSERT_TRUE(l1.empty());
+    ASSERT_THAT(l1, ::testing::ElementsAre());
 
-    int i = 0;
-    for (int e : l1) {
-        EXPECT_EQ(e, ++i);
-    }
+    ASSERT_EQ(l2.size(), 8U);
+    ASSERT_THAT(l2, ::testing::ElementsAre(1, 2, 3, 4, 5, 6, 7, 8));
 }
 
 TEST(LIST, MERGE_WITH_SWAP)
@@ -465,31 +356,35 @@ TEST(LIST, MERGE_WITH_SWAP)
 
     l2.merge(l1);
 
-    EXPECT_TRUE(l1.empty());
-    EXPECT_EQ(l2.size(), 8U);
+    ASSERT_TRUE(l1.empty());
+    ASSERT_THAT(l1, ::testing::ElementsAre());
 
-    int i = 0;
-    for (int e : l1) {
-        EXPECT_EQ(e, ++i);
-    }
+    ASSERT_EQ(l2.size(), 8U);
+    ASSERT_THAT(l2, ::testing::ElementsAre(1, 2, 3, 4, 5, 6, 7, 8));
 }
 
-TEST(LIST, MERGE_GENERIC)
+TEST(LIST, MERGE_GENERIC1)
 {
     LinkedList<Value<int>> l1{1, 2, 3, 4}, l2{0, 2, 4, 8};
 
     l2.merge(l1);
 
-    EXPECT_TRUE(l1.empty());
-    EXPECT_EQ(l2.size(), 8U);
+    ASSERT_TRUE(l1.empty());
+    ASSERT_THAT(l1, ::testing::ElementsAre());
 
-    auto it = l2.cbegin();
-    EXPECT_EQ(*it++, 0);
-    EXPECT_EQ(*it++, 1);
-    EXPECT_EQ(*it++, 2);
-    EXPECT_EQ(*it++, 2);
-    EXPECT_EQ(*it++, 3);
-    EXPECT_EQ(*it++, 4);
-    EXPECT_EQ(*it++, 4);
-    EXPECT_EQ(*it++, 8);
+    ASSERT_EQ(l2.size(), 8U);
+    ASSERT_THAT(l2, ::testing::ElementsAre(0, 1, 2, 2, 3, 4, 4, 8));
+}
+
+TEST(LIST, MERGE_GENERIC2)
+{
+    LinkedList<Value<int>> l1{0, 2, 4, 8}, l2{1, 2, 3, 4};
+
+    l2.merge(l1);
+
+    ASSERT_TRUE(l1.empty());
+    ASSERT_THAT(l1, ::testing::ElementsAre());
+
+    ASSERT_EQ(l2.size(), 8U);
+    ASSERT_THAT(l2, ::testing::ElementsAre(0, 1, 2, 2, 3, 4, 4, 8));
 }
